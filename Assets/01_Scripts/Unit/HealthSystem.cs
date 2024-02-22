767d13b409ce6fb8658f98f3261a7047bc2ae86f
LeeKiHwan <s22049@gsm.hs.kr>
2024-02-22 15:28:17 +0900

    HealthSystem 클래스 작성

---
+++ Assets/01_Scripts/Unit/HealthSystem.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    public Action<float, GameObject> OnDamaged;
+    public Action<float, GameObject> OnHealed;
+    private Action<GameObject> _onDead;
+    public Action<GameObject> OnDead
+    {
+        get => _isDead ? null : _onDead;
+        set
+        {
+            if (_isDead)
+            {
+                value.Invoke(_killer);
+            }
+            else
+            {
+                _onDead = value;
+            }
+        }
+    }
+
+    private bool _isDead = false;
+    private GameObject _killer = null;
+
+    private StatusSystem _statusSystem;
+
+    private void Awake()
+    {
+        _statusSystem = GetComponent<StatusSystem>();
+    }
+
+    public void TakeDamage(float damage, GameObject attacker)
+    {
+        if (_isDead) return;
+        if (damage > 0)
+        {
+            float origin = _statusSystem.CurrentHealth;
+            damage = Mathf.Max(damage, 0);
+
+            _statusSystem.CurrentHealth = Mathf.Max(_statusSystem.CurrentHealth - damage, 0);
+            OnDamaged?.Invoke(origin - _statusSystem.CurrentHealth, attacker);
+
+            if (_statusSystem.CurrentHealth == 0)
+            {
+                Die(attacker);
+            }
+        }
+    }
+
+    public void TakeHeal(float heal, GameObject healer)
+    {
+        if (_isDead) return;
+        if (heal > 0)
+        {
+            float origin = _statusSystem.CurrentHealth;
+            heal = Mathf.Max(heal, 0);
+
+            _statusSystem.CurrentHealth = Mathf.Min(_statusSystem.CurrentHealth + heal, _statusSystem.MaxHealth);
+
+            OnHealed?.Invoke(heal, healer);
+        }
+    }
+
+    private void Die(GameObject killer)
+    {
+        if (_isDead) return;
+        _killer = killer;
+        OnDead?.Invoke(killer);
+        _isDead = true;
+    }
+}

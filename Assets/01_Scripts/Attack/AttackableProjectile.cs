5a976b0f071461e1e16b4b60a5362784d9424c72
LeeKiHwan <s22049@gsm.hs.kr>
2024-03-13 13:42:57 +0900

    안 사용하는 using문 정리

--- Assets/01_Scripts/Attack/AttackableProjectile.cs
+++ Assets/01_Scripts/Attack/AttackableProjectile.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class AttackableProjectile : MonoBehaviour
 {

6b6923ade9087ca9390811203b025669663ca8a0
LeeKiHwan <s22049@gsm.hs.kr>
2024-04-17 18:00:34 +0900

    SkillData 스크립터블 오브젝트 클래스 작성

--- Assets/01_Scripts/Skill/SkillData.cs
+++ Assets/01_Scripts/Skill/SkillData.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkillData : ScriptableObject
+[CreateAssetMenu(fileName = "Unit Data", menuName = "Scriptable Object/Unit Data")]
+public abstract class SkillData : ScriptableObject
 {
     [Space()]
     public string SkillName;
     public int SkillLevel;
     public Sprite SkillImage;
+    public GameObject SkillPrefab;
 
     [Space()]
     public string SkillDescription;
 
+    public abstract string GetSkillDescription();
 }

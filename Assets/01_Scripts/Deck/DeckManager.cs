cbe64a32217788721e6f9ee8691a7efceb1e8b72
LeeKiHwan <s22049@gsm.hs.kr>
2024-04-17 22:45:55 +0900

    ShowHaveSkillItem 메서드 생성

--- Assets/01_Scripts/Deck/DeckManager.cs
+++ Assets/01_Scripts/Deck/DeckManager.cs
@@ -7,16 +7,22 @@ using UnityEngine.UI;
 public class DeckManager : MonoBehaviour
 {
     public static int Gold;
-
+    [Space()]
     [SerializeField] private TextMeshProUGUI _currentGoldText;
 
+    [Space()]
     [SerializeField] private Transform _haveUnitItemParent;
     [SerializeField] private GameObject _haveUnitItem;
+
+    [Space()]
+    [SerializeField] private Transform _haveSkillItemParent;
+    [SerializeField] private GameObject _haveSkillItem;
 
     private void Awake()
     {
         Gold += 10000;
         ShowHaveUnitItem();
+        ShowHaveSkillItem();
     }
 
     private void ShowHaveUnitItem()
@@ -29,6 +35,16 @@ public class DeckManager : MonoBehaviour
             {
                 Instantiate(_haveUnitItem, _haveUnitItemParent).GetComponent<HaveUnitItem>().SetItemData(unitDatas[i]);
             }
+        }
+    }
+
+    private void ShowHaveSkillItem()
+    {
+        SkillData[] skillDatas = SkillManager.Instance.GetHasSkillDatas();
+
+        for (int i=0; i<skillDatas.Length;i++)
+        {
+            Instantiate(_haveSkillItem, _haveSkillItemParent).GetComponent<HaveSkillItem>().SetItemData(skillDatas[i]);
         }
     }
 

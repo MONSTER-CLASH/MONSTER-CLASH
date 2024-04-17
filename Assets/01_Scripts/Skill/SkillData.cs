eedb5bfd316a4c960f107da8870761b08d9ec42d
LeeKiHwan <s22049@gsm.hs.kr>
2024-04-17 22:21:33 +0900

    TestSkillData 클래스 작성

--- Assets/01_Scripts/Skill/SkillData.cs
+++ Assets/01_Scripts/Skill/SkillData.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Skill Data", menuName = "Scriptable Object/Skill Data")]
 public abstract class SkillData : ScriptableObject
 {
     [Space()]
     public string SkillName;
-    public int SkillLevel;
     public Sprite SkillImage;
     public GameObject SkillPrefab;
-
-    [Space()]
-    public string SkillDescription;
+    public bool HasSkill;
 
     public abstract string GetSkillDescription();
 }

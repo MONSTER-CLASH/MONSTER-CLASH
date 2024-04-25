8830039d8fa87d695e412e497a73b3bd70be68ca
LeeKiHwan <s22049@gsm.hs.kr>
2024-04-25 17:20:19 +0900

    발악패턴 경고 표시 기능 구현

--- Assets/01_Scripts/Stage/SpecialWaveWarningShower.cs
+++ Assets/01_Scripts/Stage/SpecialWaveWarningShower.cs
@@ -6,20 +6,54 @@ public class SpecialWaveWarningShower : MonoBehaviour
 {
     public static SpecialWaveWarningShower Instance;
 
+    [SerializeField] private GameObject _warningUI;
+    [SerializeField] private Light _warningLight;
+
     private bool _isShowedWarning = false;
 
     private void Awake()
     {
         Instance = this;
+
+        ShowWarning();
     }
 
     public void ShowWarning()
     {
         if (!_isShowedWarning)
         {
-            Debug.Log("Special Wave!");
-
+            StartCoroutine(ShowWarningCoroutine());
             _isShowedWarning = true;
         }
+    }
+
+    private IEnumerator ShowWarningCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+
+        _warningUI.SetActive(true);
+        _warningLight.gameObject.SetActive(true);
+
+        for (int i=0; i<3; i++)
+        {
+            while (_warningLight.intensity < 3.5f)
+            {
+                _warningLight.intensity += Time.deltaTime * 7f;
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(0.5f);
+
+            while (_warningLight.intensity > 0)
+            {
+                _warningLight.intensity -= Time.deltaTime * 7f;
+                yield return null;
+            }
+        }
+
+        _warningUI.SetActive(false);
+        _warningLight.gameObject.SetActive(false);
+
+        yield break;
     }
 }

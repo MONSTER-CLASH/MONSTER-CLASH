a30f97bc976d629e1a420c4f2bf4594bd5c7b027
LeeKiHwan <s22049@gsm.hs.kr>
2024-04-26 10:19:21 +0900

    메인 스테이지 적군 유닛 스폰 및 발악 패턴 적용

--- Assets/01_Scripts/Unit/UnitSpawner.cs
+++ Assets/01_Scripts/Unit/UnitSpawner.cs
@@ -66,6 +66,12 @@ public class UnitSpawner : MonoBehaviour
 
         yield break;
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 0.5f);
+    }
 }
 
 [Serializable]

820515d6676b01a2720326b29ef38e60c4332747
LeeKiHwan <s22049@gsm.hs.kr>
2024-05-29 12:19:36 +0900

    원거리, 마법사 역할군 유닛 투사체 공격 구현

--- Assets/01_Scripts/Unit/Concrete Unit/Wizard/Beholder/Beholder.cs
+++ Assets/01_Scripts/Unit/Concrete Unit/Wizard/Beholder/Beholder.cs
@@ -4,4 +4,15 @@ using UnityEngine;
 
 public class Beholder : UnitController
 {
+    [SerializeField] private Transform _attackProjectileSpawnPos;
+    [SerializeField] private GameObject _attackProjectilePrefab;
+
+    protected override void HandleAttack()
+    {
+        if (_attackTarget)
+        {
+            GameObject ap = Instantiate(_attackProjectilePrefab, _attackProjectileSpawnPos.position, Quaternion.identity);
+            ap.GetComponent<AttackableProjectile>().SetProjectileData(gameObject, _attackTarget, _unitStatusSystem.AttackDamage, _unitStatusSystem.AttackRange);
+        }
+    }
 }

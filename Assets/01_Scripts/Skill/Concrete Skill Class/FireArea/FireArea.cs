ad9474df4b5194ddb3b1e901fa6de226a2d3f005
LeeKiHwan <s22049@gsm.hs.kr>
2024-05-30 10:16:01 +0900

    FireArea, FireAreaCardData 코드 구현

--- Assets/01_Scripts/Skill/Concrete Skill Class/FireArea/FireArea.cs
+++ Assets/01_Scripts/Skill/Concrete Skill Class/FireArea/FireArea.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class FireArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private FireAreaCardData _fireAreaCardData;
+    private float _damage;
+    private float _tickTime;
+    private float _destoryTime;
+
+    private void Awake()
     {
-        
+        _damage = _fireAreaCardData.Damage;
+        _tickTime = _fireAreaCardData.TickTime;
+        _destoryTime = _fireAreaCardData.DurationTime;
+
+        StartCoroutine(FireAreaCoroutine());
+        Destroy(gameObject, _destoryTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FireAreaCoroutine()
     {
-        
+        while (true)
+        {
+            Collider[] enemys = Physics.OverlapSphere(transform.position, 5, 1 << LayerMask.NameToLayer("Enemy"));
+
+            foreach (Collider enemy in enemys)
+            {
+                if (enemy.GetComponent<HealthSystem>() != null)
+                {
+                    enemy.GetComponent<HealthSystem>().TakeDamage(_damage, gameObject);
+                }
+            }
+
+            yield return new WaitForSeconds(_tickTime);
+        }
     }
 }

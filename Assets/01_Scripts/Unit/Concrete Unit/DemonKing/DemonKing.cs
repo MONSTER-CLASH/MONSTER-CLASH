e120f37a50b4818ffb1a446ed51bc67a29b8b9a5
LeeKiHwan <s22049@gsm.hs.kr>
2024-07-15 14:48:16 +0900

    마왕 유닛 구현

---
+++ Assets/01_Scripts/Unit/Concrete Unit/DemonKing/DemonKing.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonKing : UnitController
+{
+    [SerializeField] private GameObject _attackVFXPrefab;
+    [SerializeField] private GameObject _skillProjectilePrefab;
+    [SerializeField] private DemonKingCardData _demonKingCardData;
+
+    protected override void HandleAttack()
+    {
+        if (_attackTarget)
+        {
+            transform.LookAt(_attackTarget.transform);
+            Destroy(Instantiate(_attackVFXPrefab, _attackTarget.transform), 5);
+
+            Collider[] enemys = Physics.OverlapSphere(_attackTarget.transform.position, 1.5f, _oppositeLayer);
+            for (int i=0; i<enemys.Length; i++)
+            {
+                enemys[i].GetComponent<HealthSystem>().TakeDamage(_unitStatusSystem.AttackDamage, gameObject);
+            }
+        }
+    }
+
+    protected override void HandleSkill()
+    {
+        if (_canUseSkill && _attackTarget)
+        {
+            StartCoroutine(HandleSkillCoroutine());
+            _skillCool = Time.time + _demonKingCardData.SkillCoolTime;
+        }
+    }
+
+    private IEnumerator HandleSkillCoroutine()
+    {
+        transform.LookAt(_attackTarget.transform);
+        _attackTarget = null;
+        _animator.SetTrigger("Skill");
+        StartCoroutine(SetMotionStopTime());
+        yield return new WaitForSeconds(0.4f);
+
+        string enemyLayer = "";
+        if (gameObject.layer == LayerMask.NameToLayer("Player")) enemyLayer = "Enemy";
+        else if (gameObject.layer == LayerMask.NameToLayer("Enemy")) enemyLayer = "Player";
+
+        GameObject skill = Instantiate(_skillProjectilePrefab, transform.position + new Vector3(0,1.5f,0) + transform.forward, Quaternion.identity);
+        skill.GetComponent<DemonKingSkillProjectile>().SetDemonKingSkillData(gameObject, enemyLayer, _demonKingCardData.SkillDamage, _unitStatusSystem.AttackRange * 2);
+        skill.transform.forward = transform.forward;
+
+        yield break;
+    }
+}
